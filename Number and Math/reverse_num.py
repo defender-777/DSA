df4aa1022773c949b8fa56667beb679d319ddb25
Gagan R <gagan.r.2005@gmail.com>
2026-07-16 08:44:28 +0530

    Reverse a number clean code

--- Number and Math/reverse_num.py
+++ Number and Math/reverse_num.py
@@ -1,13 +1,15 @@
 def reverse(n):
+    sign = -1 if n < 0 else 1
+    n = abs(n)
 
     rev = 0
 
     while(n != 0):
         digit = n % 10
-        n = n // 10
         rev = rev * 10 + digit 
+        n = n // 10
 
-    return rev
+    return rev * sign
 
 num = int(input())
 print(reverse(num))
